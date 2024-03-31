b19278993c86231ed1c00c7ca8d13ffe4e9b214b
julien-muke <julien.muke@gmail.com>
2024-03-31 13:29:38 +0200

    🚀 project setup

---
+++ my-quiz-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizapp3a9d26ae": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
